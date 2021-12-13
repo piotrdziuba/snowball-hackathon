ad432fa17bed5fe127572823e4f2a39fc5cf1eb2
Piotr Dziuba <piotr.dziuba@allegro.pl>
2021-12-13 11:09:38 +0000

    turret

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ def move():
             return "T"
 
 
-    return moves[random.randrange(len(moves))]
+    return "R" # round turret
 
 if __name__ == "__main__":
   app.run(debug=False,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
